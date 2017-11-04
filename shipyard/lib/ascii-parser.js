69daac6c3caef900d307f7de18ef18b95198e929
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2017-11-03 23:17:09 -0700

    added demo

--- shipyard/lib/ascii-parser.js
+++ shipyard/lib/ascii-parser.js
@@ -11,37 +11,42 @@ const objects = {
     '+': 'PwrLine4'
 };
 
-function trimReduce (res, e, y) {
-    if (e.trim() !== '') {
-        const row = e.split('');
-        console.log(row);
-        const row1 = row.reduce((res1, e, x) => {
-            if (objects[e]) {
-                let cell = clone(skin[objects[e]]);
-                cell[1].transform = `translate(${20 * x},${20 * y})`;
-                res1.push(cell);
-            }
-            return res1;
-        }, ['g']);
-        res.push(row1);
-    }
+function trimReduce (res, row, y) {
+    console.log(row);
+    const row1 = row.reduce((res1, e, x) => {
+        if (objects[e]) {
+            let cell = clone(skin[objects[e]]);
+            cell[1].transform = `translate(${20 * x},${20 * y})`;
+            res1.push(cell);
+        }
+        return res1;
+    }, ['g']);
+    res.push(row1);
     return res;
 }
 
-function shipParser (str, num) {
-    const map = str.split('\n');
-    // console.log(map);
-    return map.reduce(trimReduce, ['g', {transform: `translate(0,${400 * num})`}]);
+function shipParser (arr, num) {
+    return arr.reduce(trimReduce, ['g', {
+        transform: `translate(0,${256 * num})`
+    }]);
 }
 
+const arrArr = str => {
+    const arr = str.split('\n\n\n').map(
+        ship => ship.split('\n').map(
+            row => row.split('')
+        )
+    );
+    return arr;
+};
+
 function asciiParser (str) {
-    const body = str.split('\n\n\n').map(shipParser);
-    // return onml.stringify(['svg'].concat(body));
+    const body = arrArr(str).map(shipParser);
     return onml.stringify(['svg', {
         xmlns: 'http://www.w3.org/2000/svg',
         'xmlns:inkscape': 'http://www.inkscape.org/namespaces/inkscape',
         'xmlns:sodipodi': 'http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd',
-        width: 3000, height: 3000, viewBox: [0, 0, 3000, 3000].join(' ')
+        width: 512, height: (body.length * 256), viewBox: [0, 0, 512, (body.length * 256)].join(' ')
     }].concat(body), null, 4);
 }
 
